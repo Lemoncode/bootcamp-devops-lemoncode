f3ab25c998f51c1d41ab6a740cd4d04cd6a24b50
0GiS0 <gisela.torres@returngis.net>
2020-09-30 17:40:15 +0200

    Revisado el modulo 1 y actualizado el README con el contenido de este

--- 01-contenedores/README.md
+++ 01-contenedores/README.md
@@ -1,1 +1,31 @@
-# Modulo 2 - Contenedores Docker
+# Módulo 2 - Contenedores Docker
+
+## Agenda
+
+### Día I: Introducción a Docker
+
+- ¿Por dónde empiezo?
+    * Docker Desktop
+    * Docker en Linux
+    * Docker CLI client: La línea de comandos interactiva
+- Comandos básicos:
+    * Ejecutar tu primer contenedor
+    * Introducción a las imágenes
+    * Docker Hub
+    * Exponer puertos de un contenedor
+    * ¿Dónde están los contenedores creados hasta ahora?
+    * Personalizar el nombre de los contenedores
+    * Ejecutar un contenedor y lanzar un shell interactivo en él
+    * Ejecutar comandos desde mi local dentro del contenedor
+    * Conectarme a un contenedor 
+    * Copiar archivos desde local a un contenedor y viceversa
+    * Cómo parar/iniciar un contenedor ya creado    
+    * Eliminar contenedores
+    * Docker Desktop Dashboard para la gestión de contenedores
+
+- Ejemplo práctico: SQL Server dockerizado
+- Deberes
+
+### Día II: Trabajando con imágenes
+
+- 
--- 01-contenedores/contenedores-i/contenedores-i.sh
+++ 01-contenedores/contenedores-i/contenedores-i.sh
@@ -1,22 +1,24 @@
+# Día I: Introducción a Docker #
 
-# Parte I: Introducción a los contenedores de Docker #
+#Si estás en Windows o Mac: Revisar Docker Desktop:
+https://docs.docker.com/docker-for-windows/
+https://docs.docker.com/docker-for-mac/    
+#Si trabajas con Linux elige tu distro aquí: https://docs.docker.com/engine/install/
+
+# Una vez que tienes Docker instalado en tu máquina...
 
 #Ver información del cliente y el servidor que forman Docker Engine.
-docker version 
-
-#Revisar Docker Desktop - Dashboard
+docker version
 
 #Por defecto se asume que se utilizarán contenedores Linux ( OS/Arch:  linux/amd64 en el apartado de Server).
+docker info
 
 #Si estás en Windows puedes cambiar al tipo de contenedores Windows haciendo click con el botón derecho sobre el icono de Docker en la barra de tareas y eligiendo Switch to Windows Containers
 cd c:\Program Files\Docker\Docker> .\dockercli -SwitchDaemon 
 
-docker info
-
 #### Ejecuta tu primer contenedor ####
 
 #Ejecuta tu primer contenedor
-#CLI  command  image name
 docker run hello-world
 #hello-world es la imagen que estás usando para crear tu contenedor. Una imagen es un objeto que contiene un SO, una aplicación y las dependencias que esta necesita. Si eres desarrollador puedes pensar en una imagen como si fuera una clase.
 
@@ -28,35 +30,71 @@ docker image ls
 #O bien a través del CLI
 docker search nginx
 
+#Podemos ejecutar otros contenedores con algo más de funcionalidad que el simple hello-world
+docker run nginx
+
 #### Exponer puertos en localhost (Nginx) ####
-docker run --name my-nginx -p 8080:80 nginx #Puedes ver cuando se ejecuta este comando que el terminal te muestra los logs que van surgiendo de este contenedor que acabas de crear.
+docker run --publish 8080:80 nginx #Puedes ver cuando se ejecuta este comando que el terminal te muestra los logs que van surgiendo de este contenedor que acabas de crear.
+#Normalmente, por acortar en lugar de publish utilizamos -p
+docker run -p 8080:80 nginx
 
 #Ejecutar un contenedor en segundo plano
-docker run -d --name my-nginx -p 8080:80 nginx
+docker run --detach -p 8080:80 nginx
+#o
+docker run -d -p 8080:80 nginx
+
+#Después de haber lanzado cvarios contenedores te preguntarás ¿cómo puedo ver los que tengo ahora mismo ejecutándose?
+docker ps
+
+#Pero... yo he lanzado muchos más ¿dónde están?
+docker ps --all
+#o bien
+docker ps -a
+
+#En todos los ejemplos anteriores, Docker ha elegido un nombre aleatorio para nuestros contenedores (columna NAMES). 
+#Sin embargo, muchas veces es útil poder elegir nosotros el nombre que queremos.
+#Para elegir el nombre de tu contenedor basta con utilizar la opción --name
+docker run -d --name my-web -p 9090:80 nginx
+
+#Si vuelves a listar los contenedores verás que tienes uno nuevo llamado my-web
+docker ps
+
+#También puedes renombrar existentes
+docker rename gifted_goldberg hello-world
+docker ps -a
 
 #Ejecutar un contenedor y lanzar un shell interactivo en él
 docker run -it --name my_ubuntu_container ubuntu /bin/bash
+cat /etc/os-release
+exit
 
-#### Ver qué puertos tiene un contenedor expuestos ####
-docker port my-nginx
+# Ejecutar comandos desde mi local dentro del contenedor ####
+docker exec my-web ls /var/log/nginx
 
-#### Gestionar contenedores en tu máquina ####
+#Si quiero conectarme a un contenedor
+docker run --name webserver -d nginx  #Con -d desatacho
+docker exec -it webserver bash #Ejecuto el proceso bash dentro del contenedor y con -it me atacho a él
+cat /etc/nginx/nginx.conf 
+exit
 
-#Lista los contenedores ejecutándose en tu máquina
-docker ps #¿Y los otros contenedores que he ejecutado?
 
-docker ps --all #con --all puedo ver todos los contenedores, incluso los que ya terminaron de ejecutarse, como hello-world
-docker ps -a
+## Copiar un archivo desde mi local a dentro del contenedor ####
+#https://docs.docker.com/engine/reference/commandline/cp/
+docker cp local.html my-web:/usr/share/nginx/html/local.html
+
+## Copiar el archivo de logs en local ####
+docker cp my-web:/var/log/nginx/access.log access.log
+
+## Copiar múltiples archivos dentro de una carpeta ####
+mkdir nginx-logs
+docker cp my-nginx:/var/log/nginx/. nginx-logs
+
 
 # ¿Cómo paro un contenedor?
-docker stop my-nginx
+docker stop my-web
 
 # ¿Y si quiero volver a iniciarlo?
-docker start my-nginx
-
-#Es importante hacer uso de nombres personalizados porque será más sencillo luego referirnos a él como veremos después.
-docker rename gifted_goldberg hello-world
-docker ps -a
+docker start my-web
 
 #¿Y si quiero eliminarlo del todo de mi ordenador?
 docker rm hello-world
@@ -65,30 +103,8 @@ docker ps -a #El contenedor hello-world ya no aparece en el listado
 
 #Todo esto también es posible verlo desde la interfaz de Docker Desktop (A través de la opción Dashboard)
 
-#### Demo 4: Attach ###
-#Si quiero conectarme a un contenedor
-docker run --name webserver -d nginx  #Con -d desatacho
-docker exec -it webserver bash #Ejecuto el proceso bash dentro del contenedor y con -it me atacho a él
-cat /etc/nginx/nginx.conf 
-exit
 
-#### Demo 5: Copiar un archivo desde mi local a dentro del contenedor ####
-#https://docs.docker.com/engine/reference/commandline/cp/
-docker cp local.html my-nginx:/usr/share/nginx/html/local.html
-
-#### Demo 6: Copiar el archivo de logs en local ####
-docker cp my-nginx:/var/log/nginx/access.log access.log
-
-#### Demo 7: Copiar múltiples archivos dentro de una carpeta ####
-mkdir nginx-logs
-docker cp my-nginx:/var/log/nginx/. nginx-logs
-
-
-#### Demo 8: Ejecutar comandos desde mi local dentro del contenedor ####
-docker exec my-nginx ls /var/log/nginx
-
-
-#### Demo 9: lo mismo con SQL Server ####
+## SQL Server dockerizado ####
 # Imagínate que estás desarrollando una aplicación que necesita de un SQL Server y no quieres tener que montarte uno y ensuciar tu máquina, o tener que crearte una máquina virtual, configurarla, bla, bla, bla
 # https://hub.docker.com/_/microsoft-mssql-server
 docker run --name mysqlserver -p 1433:1433 \
