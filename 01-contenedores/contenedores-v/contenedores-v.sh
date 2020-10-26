2a8874e6c99c844c869391e8cb6f1ac20a2f6b72
0GiS0 <giselatb@outlook.com>
2020-10-26 18:24:36 +0100

    separo los deberes del módulo V en un archivo aparte

--- 01-contenedores/contenedores-v/contenedores-v.sh
+++ 01-contenedores/contenedores-v/contenedores-v.sh
@@ -170,8 +170,3 @@ docker run -d alpine ping docker.com
 
 #Verás que la gráfica con la métrica engine_daemon_network_actions_seconds_count genera picos. Después de haberlo probado elimina los contenedores
 docker rm -f $(docker ps -aq)
-
-#Deberes:
-# 1. Crea un contenedor que utilice un volumen llamado datos y copia dentro de él algunas imágenes
-# 2. Elimina el contenedor anterior y comprueba que tu volumen sigue estando disponible.
-# 3. Mapea una carpeta local a un contenedor. Cambia el contenido de dicha carpeta y comprueba que ves los cambios dentro del contenedor.
