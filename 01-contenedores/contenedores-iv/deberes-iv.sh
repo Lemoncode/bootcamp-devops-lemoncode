091a97dc88f06bf3fb0a6f16dd58bae542773d8b
Gisela Torres <giselat@microsoft.com>
2020-10-27 10:20:49 +0100

    publicados las soluciones del módulo III y IV

--- 01-contenedores/contenedores-iv/deberes-iv.sh
+++ 01-contenedores/contenedores-iv/deberes-iv.sh
@@ -1,4 +1,13 @@
 #Deberes:
 # 1. Crea una nueva red de tipo bridge/nat llamada lemoncode
+docker network create lemoncode
+docker network ls
 # 2. Crea dos contenedores dentro de la red que acabas de crear, uno de ellos con la imagen nginx
+docker run -dit --name ubuntu-container --network lemoncode ubuntu bash
+docker run -dit --name nginx-container --network lemoncode nginx
 # 3. Con cURL y el nombre del contenedor solicita la web que se está ejecutando con Nginx
+docker attach ubuntu-container
+apt update && apt upgrade
+apt-get install curl -y
+curl http://nginx-container
+exit
