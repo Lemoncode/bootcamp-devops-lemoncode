93a97833d53f4f66a48cede15ef6e24ceff10136
0GiS0 <giselatb@outlook.com>
2020-10-27 18:49:12 +0100

    Deberes VI

---
+++ 01-contenedores/contenedores-vi/deberes-vi.sh
@@ -0,0 +1,4 @@
+#Deberes
+#1. Crea un cluster con Docker Swarm y con Docker Machine (3 masters y 2 workers)
+#2. Despliega un nginx con 3 replicas solo en los workers con docker service
+#3. Despliega el mismo nginx con Docker Stacks
