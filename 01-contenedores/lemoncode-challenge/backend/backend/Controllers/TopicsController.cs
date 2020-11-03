7b48c7887fcb7b53f786240afd37412e6a70db58
Gisela Torres <giselat@microsoft.com>
2020-11-03 13:26:39 +0100

    Se añade el código para los entregables de Contenedores

---
+++ 01-contenedores/lemoncode-challenge/backend/backend/Controllers/TopicsController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using backend.Models;
+using backend.Service;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicsController : ControllerBase
+    {
+        private readonly TopicService _topicService;
+
+        public TopicsController(TopicService topicService)
+        {
+            _topicService = topicService;
+        }
+
+        // GET: api/<TopicsController>
+        [HttpGet]
+        public IEnumerable<Topic> Get() => _topicService.Get();
+
+
+        // GET api/<TopicsController>/5
+        [HttpGet("{id:length(24)}", Name = "GetTopic")]
+        public ActionResult<Topic> Get(string id)
+        {
+            var topic = _topicService.Get(id);
+
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return topic;
+        }
+
+        // POST api/<TopicsController>
+        [HttpPost]
+        public ActionResult<Topic> Create(Topic topic)
+        {
+            _topicService.Create(topic);
+
+            return topic;
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Topic topicIn)
+        {
+            var topic = _topicService.Get(id);
+
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            _topicService.Update(id, topicIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var topic = _topicService.Get(id);
+
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            _topicService.Remove(topic.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ 01-contenedores/lemoncode-challenge/backend/backend/Startup.cs
@@ -0,0 +1,59 @@
+using backend.Models;
+using backend.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //requires using Microsoft.Extensions.Options
+            services.Configure<TopicstoreDatabaseSettings>(Configuration.GetSection(nameof(TopicstoreDatabaseSettings)));
+            services.AddSingleton<ITopicstoreDatabaseSettings>(sp => sp.GetRequiredService<IOptions<TopicstoreDatabaseSettings>>().Value);
+            services.AddSingleton<TopicService>();
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors("MyPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
