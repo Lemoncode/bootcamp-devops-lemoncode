721d1b4e1564b95e620a532901a141a7423e09d6
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2020-12-02 20:08:11 +0100

    added start script for Jenkins

--- 03-cd/01-jenkins/00-instalando-jenkins/start_jenkins.sh
+++ 03-cd/01-jenkins/00-instalando-jenkins/start_jenkins.sh
@@ -1,6 +1,26 @@
+#!/bin/bash
 IMAGE=$1
-CERTS_VOLUME=$2
-DATA_VOLUME=$3
+NETWORK=$2
+CERTS_VOLUME=$3
+DATA_VOLUME=$4
+
+# Reference: https://forums.docker.com/t/how-to-filter-docker-ps-by-exact-name/2880/3
+
+if [[ $(docker network ls --filter name=^/$NETWORK$) ]]; then
+  echo 'hello'
+  docker network create jenkins
+  echo 'network jenkins created'
+fi
+
+if [[ $(docker volume ls --filter name=^/$CERTS_VOLUME$) ]]; then
+  docker volume create $CERTS_VOLUME
+  echo network jenkins "$CERTS_VOLUME"
+fi 
+
+if [[ $(docker volume ls --filter name=^/$CERTS_VOLUME$) ]]; then
+  docker volume create $DATA_VOLUME
+  echo network jenkins "$DATA_VOLUME"
+fi 
 
 # StartDocker in Docker into jenkins network
 docker container run --name jenkins-docker --rm --detach \
@@ -17,3 +37,8 @@ docker container run --name jenkins-blueocean --rm --detach \
   --volume "$DATA_VOLUME":/var/jenkins_home \
   --volume "$CERTS_VOLUME":/certs/client:ro \
   --publish 8080:8080 --publish 50000:50000 "$IMAGE"
+
+# Ensure that script have permissions to be executed: chmod +x start_jenkins.sh
+# Build image from Dockerfile: docker build -t lemoncode/jenkins .
+# From previous image we can run custom Jenkins version as follows:
+# ./start_jenkins.sh lemoncode/jenkins jenkins jenkins-docker-certs jenkins-data
