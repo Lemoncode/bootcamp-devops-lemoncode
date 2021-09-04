4b85d9fdad13a815928fb382d6123e6629d8d8d2
0gis0 <giselatb@outlook.com>
2021-09-04 13:18:56 +0200

    actualizada la resolución de los deberes

--- 01-contenedores/contenedores-v/deberes-v.sh
+++ 01-contenedores/contenedores-v/deberes-v.sh
@@ -4,13 +4,9 @@ docker network create lemoncode
 docker network ls
 # 2. Crea dos contenedores dentro de la red que acabas de crear, uno de ellos con la imagen nginx
 docker run -dit --name ubuntu-container --network lemoncode ubuntu bash
-docker run -dit --name nginx-container --network lemoncode nginx
+docker run -d --name nginx-container --network lemoncode nginx
 # 3. Con cURL y el nombre del contenedor solicita la web que se está ejecutando con Nginx
 docker attach ubuntu-container
-apt update && apt upgrade
-apt-get install curl -y
+apt update && apt upgrade && apt -y install curl
 curl http://nginx-container
 exit
-
-
-docker run -d --name frontend -p 3000:3000 --network lemoncode-challenge frontend
