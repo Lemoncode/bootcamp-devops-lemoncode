00fc27661fff71529baf9b13564fb6390a4a9db7
Gisela <giselatb@outlook.com>
2022-01-09 17:58:24 +0100

    update demo 0

--- 04-cloud/00-aks/00-mi-primer-aks/00-crear-mi-primer-aks.sh
+++ 04-cloud/00-aks/00-mi-primer-aks/00-crear-mi-primer-aks.sh
@@ -7,20 +7,24 @@ brew install azure-cli
 #También podemos crear un contenedor con Azure CLI. Así no tenemos que instalarlo en nuestro local ;-)
 docker run -it --rm microsoft/azure-cli sh
 
+# Para ver el subconjunto de serviios que puedes gestionar a través de la linea de comandos:
+az
+
 #Iniciamos sesión en nuestra cuenta de Azure
 az login
 
 #Creamos un grupo de recursos en una ubicación concreta
-RESOURCE_GROUP="Mi-Primer-AKS"
+RESOURCE_GROUP="Lemoncode-CLI"
 LOCATION="northeurope"
 
 az group create -n ${RESOURCE_GROUP} -l ${LOCATION}
 
 #Creamos el clúster de AKS
-AKS_NAME="lemoncode-aks"
+AKS_NAME="aks-lemoncode-cli"
 
 #https://docs.microsoft.com/en-us/cli/azure/aks?view=azure-cli-latest#az_aks_create
-az aks create -g ${RESOURCE_GROUP} -n ${AKS_NAME} \
+az aks create -g ${RESOURCE_GROUP} \
+-n ${AKS_NAME} \
 --node-count 1 --generate-ssh-keys
 
 #Instalamos kubectl en local si no lo tenemos. En este caso en el contenedor con Azure CLI
@@ -32,14 +36,17 @@ az aks get-credentials -g ${RESOURCE_GROUP} -n ${AKS_NAME}
 #Recuperamos los nodos de nuestro clúster (en este ejemplo solo deberíamos de tener 1)
 kubectl get nodes
 
-#Recuperamos todos los servicios desplegados en nuestro clúster
-kubectl get services --all-namespaces
-
 #Escalar el número de nodos en el clúster
 az aks scale -g ${RESOURCE_GROUP} -n ${AKS_NAME} --node-count 3
 
 #Ahora deberíamos tener 3 nodos en lugar de 1
 kubectl get nodes
+
+# Crear un Wordpress en nuestro clúster
+kubectl apply -f wordpress.yaml
+
+# Comprobar lo que acabamos de desplegar
+kubectl get all -n wordpress
 
 #Si eliminamos el grupo de recursos eliminaremos el clúster
 az group delete -n ${RESOURCE_GROUP} --yes --no-wait
