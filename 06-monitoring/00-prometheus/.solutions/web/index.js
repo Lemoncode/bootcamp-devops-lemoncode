ea94d158e67f9f64da4b97438b0cc1adcffb59c3
JaimeSalas <jaime.salas@lemoncode.net>
2022-01-17 18:45:18 +0100

    added app solutions

--- 06-monitoring/00-prometheus/.solutions/web/index.js
+++ 06-monitoring/00-prometheus/.solutions/web/index.js
@@ -1,15 +1,44 @@
 const express = require('express');
+const promClient = require('prom-client');
 const { port } = require('./config');
-const { getQuote } = require('./helpers');
+const { getQuote, delay, getRandom } = require('./helpers');
+const { counter, histogram, delaySummary } = require('./custom-metrics');
+
+const { register, collectDefaultMetrics } = promClient;
+
+collectDefaultMetrics();
 
 const app = express();
 
-// curl -X GET "http://localhost:3000/quote"
-app.get('/quote', async (_, res) => {
+// curl http://localhost:3000/metrics
+app.get('/metrics', async (_, res) => {
   try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
+});
+
+
+// curl -X GET "http://localhost:3000/quote?slow=true"
+app.get('/quote', async (req, res) => {
+  try {
+    counter.labels('GET', 200).inc();
+    const end = histogram.startTimer();
     const quote = getQuote();
+    const { slow } = req.query;
+
+    if (slow === 'true') {
+      const delaySeconds = getRandom(2, 10);
+      delaySummary.observe(delaySeconds);
+      await delay(delaySeconds);
+    }
+
+    histogram.labels(200).observe(end());
     res.send(quote);
   } catch (ex) {
+    counter.labels('GET', 500).inc();
     res.status(500).end(ex);
   }
 });
