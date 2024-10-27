9cc228bd10dc89a060d861d74cc6501227d38878
Jaime Salas <jaime.salas@lemoncode.net>
2024-10-27 21:44:23 +0100

    updated frontend

--- 01-contenedores/lemoncode-challenge/node-stack/frontend/server.js
+++ 01-contenedores/lemoncode-challenge/node-stack/frontend/server.js
@@ -7,14 +7,14 @@ const LOCAL = 'http://localhost:5000/api/topics';
 
 app.set('view engine', 'ejs');
 
-app.get('/', async (req, res) => {
+app.get('/', async (_, res) => {
 
     //Recuperar topics de la API
     const response = await fetch(process.env.API_URI || LOCAL);
     const topics = await response.json();
 
-    res.render('index', { topics });
 
+    res.render('index', { topics });
 });
 
 app.listen(3000, () => {
